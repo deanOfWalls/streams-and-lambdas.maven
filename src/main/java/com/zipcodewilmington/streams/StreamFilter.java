446daf49fab9e8ef0f59c6b2f1058d67695cee64
deanOfWalls <deanofwalls@gmail.com>
2023-04-14 18:24:44 -0400

    checkpointing

--- src/main/java/com/zipcodewilmington/streams/StreamFilter.java
+++ src/main/java/com/zipcodewilmington/streams/StreamFilter.java
@@ -20,7 +20,7 @@ public class StreamFilter {
      * No arg constructor
      */ //TODO - construct person stream of 100 person objects; startingCharacter is a random capital letter
     public StreamFilter() {
-        this(Stream.empty(), null);
+        this(new PersonFactory().createPersonStream(100), (char) (Math.random() * (90 - 65) + 65));
     }
 
     /**
@@ -28,7 +28,7 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(Person[] people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this.
     }
 
     /**
@@ -55,7 +55,7 @@ public class StreamFilter {
      * @return a list of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListMultiLine() {
-        return null;
+        return personStream.filter((person -> person.getName().startsWith(startingCharacter))).collect)(Collectors.toList());
     }
 
 
@@ -64,7 +64,8 @@ public class StreamFilter {
      * @return a list of person objects whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListOneLine() {
-        return null;
+        return personStream.filter((person -> person.getName());
+
     }
 
 
@@ -73,7 +74,7 @@ public class StreamFilter {
      * @return an array of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public Person[] toArrayOneLine() {
-        return null;
+        return toListOneLine().toArray(new Person[0]);
     }
 
 
@@ -82,7 +83,7 @@ public class StreamFilter {
      * @return an array of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public Person[] toArrayMultiLine() {
-        return null;
+        return toListMultiLine().toArray(new Person[0]);
     }
 
 }
